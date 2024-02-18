aa4a1be92677a73079c26d0e842738b48fa2d703
kimGyeongHa <rudgkrk11@naver.com>
2024-02-18 16:44:00 +0900

    fix : merge error

--- src/main/java/jpabook/jpashop/domain/Locker.java
+++ src/main/java/jpabook/jpashop/domain/Locker.java
@@ -1,17 +1,10 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
-<<<<<<< HEAD
-=======
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
->>>>>>> 4e7a4fbdb5456af5b83dc1415a0a6e40deb5535c
 
 
 @Entity
 public class Locker {
-<<<<<<< HEAD
     @Id @Column(name = "LOCKER_ID")
     @GeneratedValue
     private int id;
@@ -44,12 +37,4 @@ public class Locker {
     public void setName(String name) {
         this.name = name;
     }
-=======
-    @Id@GeneratedValue
-    private Long id;
-    private String name;
-
-    @OneToOne(mappedBy = "locker")
-    private Member member;
->>>>>>> 4e7a4fbdb5456af5b83dc1415a0a6e40deb5535c
 }
--- src/main/java/jpabook/jpashop/domain/Movie.java
+++ src/main/java/jpabook/jpashop/domain/Movie.java
@@ -2,15 +2,9 @@ package jpabook.jpashop.domain;
 import javax.persistence.Entity;
 
 @Entity
-<<<<<<< HEAD
-public class Movie extends Item{
-    private String movie_name;
-    private int movie_price;
-=======
 public class Movie extends Item {
     private String director;
     private String actor;
->>>>>>> 4e7a4fbdb5456af5b83dc1415a0a6e40deb5535c
 
     public String getDirector() {
         return director;
